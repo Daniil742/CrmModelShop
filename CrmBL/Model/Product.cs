b9f47e1b31e297cf1d055b8d430f325857ce5906
Timofeev Daniil <timofeev742@gmail.com>
2022-02-05 22:50:04 +0400

    Добавлены пара моделей и тесты к одной из них(Cart)

--- CrmBL/Model/Product.cs
+++ CrmBL/Model/Product.cs
@@ -9,9 +9,24 @@ namespace CrmBL.Model
         public decimal Price { get; set; }
         public int Count { get; set; }
         public virtual ICollection<Sell> Sells { get; set; }
+
         public override string ToString()
         {
             return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Product product)
+            {
+                return ProductId.Equals(product.ProductId);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ProductId;
         }
     }
 }
