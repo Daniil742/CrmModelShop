6af165f30c7b67f46af0a17af282131e9ecb2d0e
Timofeev Daniil <timofeev742@gmail.com>
2022-02-05 18:25:27 +0400

    Добавьте файлы проекта.

---
+++ CrmUI/ProductForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace CrmUI
+{
+    partial class ProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxProductName = new System.Windows.Forms.TextBox();
+            this.buttonProductAdd = new System.Windows.Forms.Button();
+            this.numericUpDownPrice = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownCount = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите наименование";
+            // 
+            // textBoxProductName
+            // 
+            this.textBoxProductName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxProductName.Location = new System.Drawing.Point(181, 4);
+            this.textBoxProductName.Name = "textBoxProductName";
+            this.textBoxProductName.Size = new System.Drawing.Size(227, 22);
+            this.textBoxProductName.TabIndex = 1;
+            // 
+            // buttonProductAdd
+            // 
+            this.buttonProductAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonProductAdd.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonProductAdd.Location = new System.Drawing.Point(321, 229);
+            this.buttonProductAdd.Name = "buttonProductAdd";
+            this.buttonProductAdd.Size = new System.Drawing.Size(88, 29);
+            this.buttonProductAdd.TabIndex = 2;
+            this.buttonProductAdd.Text = "Добавить";
+            this.buttonProductAdd.UseVisualStyleBackColor = true;
+            this.buttonProductAdd.Click += new System.EventHandler(this.buttonProductAdd_Click);
+            // 
+            // numericUpDownPrice
+            // 
+            this.numericUpDownPrice.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownPrice.DecimalPlaces = 2;
+            this.numericUpDownPrice.Location = new System.Drawing.Point(181, 32);
+            this.numericUpDownPrice.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.numericUpDownPrice.Name = "numericUpDownPrice";
+            this.numericUpDownPrice.Size = new System.Drawing.Size(227, 22);
+            this.numericUpDownPrice.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите стоимость";
+            // 
+            // numericUpDownCount
+            // 
+            this.numericUpDownCount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownCount.Location = new System.Drawing.Point(181, 61);
+            this.numericUpDownCount.Name = "numericUpDownCount";
+            this.numericUpDownCount.Size = new System.Drawing.Size(228, 22);
+            this.numericUpDownCount.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Введите количество";
+            // 
+            // ProductForm
+            // 
+            this.AcceptButton = this.buttonProductAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 270);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDownCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDownPrice);
+            this.Controls.Add(this.buttonProductAdd);
+            this.Controls.Add(this.textBoxProductName);
+            this.Controls.Add(this.label1);
+            this.Name = "ProductForm";
+            this.Text = "ProductForm";
+            this.Load += new System.EventHandler(this.ProductForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxProductName;
+        private System.Windows.Forms.Button buttonProductAdd;
+        private System.Windows.Forms.NumericUpDown numericUpDownPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownCount;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ CrmUI/SellerForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace CrmUI
+{
+    partial class SellerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSellerAdd = new System.Windows.Forms.Button();
+            this.textBoxSellerName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSellerAdd
+            // 
+            this.buttonSellerAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSellerAdd.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonSellerAdd.Location = new System.Drawing.Point(249, 229);
+            this.buttonSellerAdd.Name = "buttonSellerAdd";
+            this.buttonSellerAdd.Size = new System.Drawing.Size(88, 29);
+            this.buttonSellerAdd.TabIndex = 0;
+            this.buttonSellerAdd.Text = "Добавить";
+            this.buttonSellerAdd.UseVisualStyleBackColor = true;
+            this.buttonSellerAdd.Click += new System.EventHandler(this.buttonSellerAdd_Click);
+            // 
+            // textBoxSellerName
+            // 
+            this.textBoxSellerName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSellerName.Location = new System.Drawing.Point(110, 4);
+            this.textBoxSellerName.Name = "textBoxSellerName";
+            this.textBoxSellerName.Size = new System.Drawing.Size(227, 22);
+            this.textBoxSellerName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Введите имя";
+            // 
+            // SellerForm
+            // 
+            this.AcceptButton = this.buttonSellerAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 270);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSellerName);
+            this.Controls.Add(this.buttonSellerAdd);
+            this.Name = "SellerForm";
+            this.Text = "SellerForm";
+            this.Load += new System.EventHandler(this.SellerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonSellerAdd;
+        private System.Windows.Forms.TextBox textBoxSellerName;
+        private System.Windows.Forms.Label label2;
+    }
+}
